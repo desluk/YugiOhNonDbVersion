777f211a2c4e3ec34231e3c9a954a7f846ba085b
desluk <36411526+desluk@users.noreply.github.com>
2023-02-10 21:10:22 +0200

    The start of the radio button additions

--- YugiOh_NonDBVersion/Models/YugiOhCardViewModel.cs
+++ YugiOh_NonDBVersion/Models/YugiOhCardViewModel.cs
@@ -4,5 +4,8 @@ public class YugiOhCardViewModel
 {
     public string name { get; set; }
     public string descrtion { get; set; }
+    public int[] prices { get; set; }
+    public List<string> sets { get; set; }
     public List<byte[]> smallCardImages { get; set; }
+    public List<string> searchTypes { get; set; }
 }
